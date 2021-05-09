fd2a6d586ff16e83d57901bc32fccc492c0ef188
Swaraj Gandhi <swaraj@browserstack.com>
2021-05-09 16:51:59 +0530

    added icons structure for the ironlist

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+"use strict";var _IconsBasicLocationPinThick=_interopRequireDefault(require("./IconsBasicLocationPinThick"));Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"IconsBasicLocationPinThick",{enumerable:!0,get:function get(){return _IconsBasicLocationPinThick.default}});function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}
---
+++ src/icons-dest/index.js
@@ -0,0 +1,1 @@
+export { default as IconsBasicLocationPinThick } from './IconsBasicLocationPinThick'
